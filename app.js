b797a030616883783e901532ccd5d0adc55eb0c8
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-12 14:17:15 +0100

    Created two GET routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
 
+
 // routes middleware
 app.use('/api/v1/', userRouter);
 
--- routes/users/index.js
+++ routes/users/index.js
@@ -8,4 +8,12 @@ userRouter.get('/users', (req, res) => {
     res.status(200).send('Hello World! From the express_routes app!');
 });
 
+userRouter.get('/users/courses', (req, res) => {
+    res.send([1, 2, 3, 4]);
+})
+
+userRouter.get('/users/courses/:id', (req, res) => {
+    res.send(req.params.id);
+})
+
 module.exports = userRouter;
