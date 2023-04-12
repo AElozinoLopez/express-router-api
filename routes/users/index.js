488058eb3da792da680a884f92f11801b89ca0cd
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-12 21:52:51 +0100

    Created more routes and applied HTTP verbs for practice purpose

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,17 +3,44 @@ const express = require('express');
 // create router
 const userRouter = express.Router();
 
+const courses = [
+    {id:1, name: 'course1'},
+    {id:2, name: 'course2'},
+    {id:3, name: 'course3'}
+];
+
 // GET / Home page (create it)
 userRouter.get('/users', (req, res) => {
     res.status(200).send('Hello World! From the express_routes app!');
 });
 
 userRouter.get('/users/courses', (req, res) => {
-    res.send([1, 2, 3, 4]);
+    res.send(courses);
 })
 
 userRouter.get('/users/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course)
 })
+
+
+userRouter.post ('/users/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+})
+
+
+// userRouter.get('/users/courses/:id', (req, res) => {
+//     res.send(req.params.id);
+// })
+
+// userRouter.get('/users/courses/:name/:year', (req, res) =>  {
+//     res.send(req.query);
+// })
 
 module.exports = userRouter;
